9f6c7c831caa789565f48ab515d990a2897698b0
Youssef60545 <youssefguzman78@gmail.com>
2025-03-28 15:26:05 -0600

    Update Practica_2.py
    Documenté algunas lineas de código

--- Practica_2.py
+++ Practica_2.py
@@ -9,20 +9,20 @@ import csv  #se usa para leer y escribir archivos en formato CSV
 import time time  #proporciona funciones para manejar el tiempo y pausas en la ejecución del código.
 
 # Configurar el puerto serial 
-PUERTO_SERIAL = "COM3" 
-BAUD_RATE = 9600
-ARCHIVO = "datos_sensores.csv"
+PUERTO_SERIAL = "COM3" # Define el puerto donde está conectado el Arduino (cambiar si es necesario)
+BAUD_RATE = 9600 # Velocidad de comunicación serial en baudios (debe coincidir con el valor en Arduino)
+ARCHIVO = "datos_sensores.csv" # Nombre del archivo donde se guardarán los datos
 
 # Abrir conexión serial
-arduino = serial.Serial(PUERTO_SERIAL, BAUD_RATE, timeout=1)
-time.sleep(2)  # Esperar a que el Arduino inicie
-
+arduino = serial.Serial(PUERTO_SERIAL, BAUD_RATE, timeout=1) # Establece la conexión con el puerto serie
+time.sleep(2)  # Esperar a que el Arduino inicie 
 # Crear y abrir el archivo CSV
 with open(ARCHIVO, mode="w", newline="") as file: : #Abre un archivo con nombre ARCHIVO,Si el archivo ya existe, se sobrescribirá,Usa with, lo que garantiza que el archivo se cierre automáticamente.
     escribir = csv.writer(file) # permite escribir datos en un archivo de manera estructurada
     escribir.writerow(["Potenciometro", "Voltaje (V)", "Temperatura (°C)", "Humedad (%)"])  # Encabezados
 
     try:
+         # Bucle infinito para leer datos continuamente desde Arduino
         while True:
             linea = arduino.readline().decode().strip()  # Leer línea y decodificar
             if linea:
